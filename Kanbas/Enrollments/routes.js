5aeb896df51ece7f0e2796e0ed698f1686924b69
Mengzuqing <118959603+Mengzuqing@users.noreply.github.com>
2024-12-12 05:22:36 -0800

    Delete Kanbas directory

--- Kanbas/Enrollments/routes.js
+++
@@ -1,9 +0,0 @@
-import * as dao from "./dao.js";
-
-export default function EnrollmentsRoutes(app) {
-  const getAllEnrollments = (req, res) => {
-    const enrollments = dao.getAllEnrollents();
-    res.json(enrollments);
-  };
-  app.get("/api/enrollments", getAllEnrollments);
-}
